b8428a10381ddfaf1ba75704e7a2653d537b6315
unknown <filipstoianov@abv.bg>
2023-05-21 17:04:32 +0300

    DB connection fixes

--- server/db/db.php
+++ server/db/db.php
@@ -35,7 +35,12 @@ class DataBaseConnection
     {
         try {
             $db_credentials = parse_ini_file("config.ini", true);
-            $this->connection = new PDO('mysql:host=localhost; dbname=ascii_art', $db_credentials["user"], $db_credentials["password"]);
+            $db_host = getenv('DB_HOST');
+            $db_name = getenv('DB_NAME');
+            $db_user = getenv('DB_USER');
+            $db_password = getenv('DB_PASSWORD');
+
+            $this->connection = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_password);
 
             $this->prepareSQLStatements();
         } catch (PDOException $error) {
